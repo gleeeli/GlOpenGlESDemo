261d4afa8d5b3c1e55915eb2fed035fc2b043a72
liguanglei <liguanglei@boyue.com>
2019-05-17 18:06:25 +0800

    小修改
    小修改

--- GlOpenGlESDemo/GlStudyDemo/Light/LightColors/LightColorsViewController.mm
+++ GlOpenGlESDemo/GlStudyDemo/Light/LightColors/LightColorsViewController.mm
@@ -40,11 +40,9 @@
     [self handleVertex];
 }
 
-- (void)handleVertex {
-    NSLog(@"handle vertex");
-    
+- (void)getVertices:(float *)vertices {
     //一共需要36个顶点（6个面 x 每个面有2个三角形组成 x 每个三角形有3个顶点）
-    float vertices[] = {
+    *vertices[] = {
         // z轴上的两个平行面
         //前平面
         0.5f,  0.5f, 0.0f,    // top right
@@ -96,6 +94,67 @@
         -0.5f,  -0.5f, -0.5f,   // top left
         0.5f,  -0.5f, -0.5f, // top right
     };
+
+}
+
+- (void)handleVertex {
+    NSLog(@"handle vertex");
+    //18 * 6 = 108;
+    float *vertices = malloc(108 * sizeof(float*));
+    
+//    //一共需要36个顶点（6个面 x 每个面有2个三角形组成 x 每个三角形有3个顶点）
+//    float vertices[] = {
+//        // z轴上的两个平行面
+//        //前平面
+//        0.5f,  0.5f, 0.0f,    // top right
+//        0.5f, -0.5f, 0.0f,   // bottom right
+//        -0.5f, -0.5f, 0.0f,   // bottom left
+//        -0.5f, -0.5f, 0.0f,   // bottom left
+//        -0.5f,  0.5f, 0.0f,    // top left
+//        0.5f,  0.5f, 0.0f,    // top right
+//
+//        //后平面
+//        0.5f,  0.5f, -0.5f,    // top right
+//        0.5f, -0.5f, -0.5f,    // bottom right
+//        -0.5f, -0.5f, -0.5f,  // bottom left
+//        -0.5f, -0.5f, -0.5f,   // bottom left
+//        -0.5f,  0.5f, -0.5f,    // top left
+//        0.5f,  0.5f, -0.5f,   // top right
+//
+//        // x轴上的两个平行面
+//        //右平面
+//        0.5f,  0.5f, -0.5f,   // top right
+//        0.5f, -0.5f, -0.5f,   // bottom right
+//        0.5f, -0.5f, 0.0f,  // bottom left
+//        0.5f, -0.5f, 0.0f,   // bottom left
+//        0.5f,  0.5f, 0.0f,   // top left
+//        0.5f,  0.5f, -0.5f,  // top right
+//
+//        //左平面 留个缺口，不留缺口可将y轴的0.0f改为-0.5f
+//        -0.5f,  0.5f, -0.5f,  // top right
+//        -0.5f, 0.0f, -0.5f,   // bottom right
+//        -0.5f, 0.0f, 0.0f,  // bottom left
+//        -0.5f, 0.0f, 0.0f,  // bottom left
+//        -0.5f,  0.5f, 0.0f,   // top left
+//        -0.5f,  0.5f, -0.5f,   // top right
+//
+//        // y轴上的两个平行面
+//        //上平面
+//        0.5f,  0.5f, -0.5f,  // top right
+//        0.5f, 0.5f, 0.0f,  // bottom right
+//        -0.5f, 0.5f, 0.0f,  // bottom left
+//        -0.5f, 0.5f, 0.0f,  // bottom left
+//        -0.5f,  0.5f, -0.5f,   // top left
+//        0.5f,  0.5f, -0.5f,  // top right
+//
+//        //下平面
+//        0.5f,  -0.5f, -0.5f,  // top right
+//        0.5f, -0.5f, 0.0f,    // bottom right
+//        -0.5f, -0.5f, 0.0f,   // bottom left
+//        -0.5f, -0.5f, 0.0f,   // bottom left
+//        -0.5f,  -0.5f, -0.5f,   // top left
+//        0.5f,  -0.5f, -0.5f, // top right
+//    };
     glGenVertexArraysOES(1, &cubeVAO);
     glGenBuffers(1, &VBO);
     
